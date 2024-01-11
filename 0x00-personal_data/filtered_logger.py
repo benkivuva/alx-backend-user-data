47514b7826990692e2cdee77d5d1624e6f3ae27c
benkivuva <benkivuvak5@gmail.com>
2024-01-10 23:10:45 -0800

    update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
 """
-This project module contains several fucntions
-to work with the personal data
+This project module contains a logging module
 """
 import re
+import logging
+from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize the class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Method to filter values in incoming log records using filter_datum.
+            Returns: A log.
+        """
+        msg = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
 
 def filter_datum(fields, redaction, message, separator):
-    #result = re.sub(fields, redaction, message)
-    for f in fields:
-        result = re.sub(f, redaction, message) 
-    """print(fields)
-    print(type(fields))
-    print(redaction)
-    print(type(redaction))
-    print(message)
-    print(type(message))
-    print(separator)
-    print(type(separator))
-    #return message"""
-    return result
+    """
+    This is a function that uses a regex to replace
+    occurrences of certain field values.
+    Returns:
+        The log message obfuscated.
+    """
+    return (separator.join(x if x.split('=')[0] not in fields else re.sub(
+        r'=.*', '=' + redaction, x) for x in message.split(separator)))
