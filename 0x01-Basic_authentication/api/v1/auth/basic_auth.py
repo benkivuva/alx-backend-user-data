e26c608845d3392baff241ee8fa26a9981649b8d
benkivuva <benkivuvak5@gmail.com>
2024-01-17 14:58:11 +0300

    Basic - Base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,3 +13,16 @@ class BasicAuth(Auth):
         """ Constructor
         """
         pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+
+        start_auth = authorization_header[:6]
+        base64_auth = authorization_header[6:]
+
+        if start_auth != "Basic ":
+            return None
+        return base64_auth
