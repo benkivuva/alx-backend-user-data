ad30d9a2484173fa7463f324127c374280700790
benkivuva <benkivuvak5@gmail.com>
2024-01-17 15:09:26 +0300

    decode_base64 added

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Class BasicAuth
 """
 from api.v1.auth.auth import Auth
+import base64
+import codecs
 
 
 class BasicAuth(Auth):
@@ -12,17 +14,36 @@ class BasicAuth(Auth):
     def __init__(self):
         """ Constructor
         """
-        pass
+        super().__init__()
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """ Method that takes the authorization header and
+        returns the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+
         if authorization_header is None or not isinstance(
                 authorization_header, str):
             return None
 
-        start_auth = authorization_header[:6]
-        base64_auth = authorization_header[6:]
-
-        if start_auth != "Basic ":
+        if not authorization_header.startswith("Basic "):
             return None
-        return base64_auth
+        return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Method that takes the base64_authorization header and
+        returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            string_bytes = base64.b64decode(base64_bytes)
+            return string_bytes.decode('utf-8')
+        except Exception as e:
+            return None
