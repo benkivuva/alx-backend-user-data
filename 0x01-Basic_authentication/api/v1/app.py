67c269690f96414b253df213c28f1bd3937a6324
benkivuva <benkivuvak5@gmail.com>
2024-01-17 14:52:54 +0300

    basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,37 +4,20 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request, make_response
+from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
-auth_type = os.getenv('AUTH_TYPE')
+auth = os.getenv('AUTH_TYPE')
 
-if auth_type:
+if auth:
     from api.v1.auth.auth import Auth
     auth = Auth()
-
-
-@app.before_request
-def before_request():
-    """Method that filters authorization to each request"""
-    auth_list = [
-        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
-    ]
-    if auth is None:
-        return
-    check_auth = auth.require_auth(request.path, auth_list)
-    if not check_auth:
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
 
 
 @app.errorhandler(404)
@@ -46,16 +29,34 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def request_unauthorized(error) -> str:
-    """ Unauthorized user handler
+    """ unauthorized user handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden access handler
+    """ forbidden access handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """Executed before each request that is handled by a function of the
+    Blueprint."""
+    excluded_paths = [
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+    ]
+    if auth is None:
+        return
+    check_auth = auth.require_auth(request.path, excluded_paths)
+    if check_auth is False:
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
