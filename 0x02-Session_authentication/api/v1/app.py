9a58b226114bf299b9decbbad2b099c5842d3040
benkivuva <benkivuvak5@gmail.com>
2024-01-18 10:30:43 +0300

    current_user added

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -15,9 +16,16 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth = os.getenv('AUTH_TYPE')
 
-if auth:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+if auth == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif auth == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    if auth:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 
 
 @app.errorhandler(404)
@@ -44,19 +52,23 @@ def forbidden(error) -> str:
 @app.before_request
 def before_request():
     """Executed before each request that is handled by a function of the
-    Blueprint"""
+    Blueprint."""
     excluded_paths = [
-        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth is None:
         return
     check_auth = auth.require_auth(request.path, excluded_paths)
     if check_auth is False:
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    else:
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
