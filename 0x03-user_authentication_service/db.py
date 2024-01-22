d3232f414cbe34118e1c3e124f3fc6fc67be41cc
benkivuva <benkivuvak5@gmail.com>
2024-01-22 11:39:10 +0300

    update find_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,10 +1,17 @@
+#!/usr/bin/python3
 """DB module
 """
+from gettext import find
+from requests import session
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import Base, User  # Assuming User class is defined in user module
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+from user import Base
+from user import User
 
 
 class DB:
@@ -29,8 +36,33 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database."""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
+        """
+        Method that takes two arguments to save the user to the database.
+            Returns: A User object.
+        """
+        if email and hashed_password:
+            user = User(
+                email=email, hashed_password=hashed_password
+            )
+            self._session.add(user)
+            self._session.commit()
+            return user
+        return None
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Method that takes in arbitrary keyword arguments.
+            Returns: The first row found in the users table as filtered
+        by the passed arguments.
+        """
+        valid_arguments = [
+            'id', 'email', 'hashed_password', 'session_id', 'reset_token'
+        ]
+        input_keys = kwargs.keys()
+        for k in input_keys:
+            if k not in valid_arguments:
+                raise InvalidRequestError
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
