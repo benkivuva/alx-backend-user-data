bdcd60e8d819d77cc700c9a7feeac11cc84626bd
benkivuva <benkivuvak5@gmail.com>
2024-01-22 11:08:05 +0300

    docstring update

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 """
-Contains SQLAlchemy model named User
+Contains SQLAlchemy model named User.
 """
-
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
+from typing import Optional
 
 Base = declarative_base()
 
 
 class User(Base):
-    """Representation of a user """
+    """
+    Representation of a user.
+
+    Attributes:
+    - id: int, primary key
+    - email: str, non-nullable
+    - hashed_password: str, non-nullable
+    - session_id: Optional[str], nullable
+    - reset_token: Optional[str], nullable
+    """
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: Optional[str] = Column(String(250), nullable=True)
+    reset_token: Optional[str] = Column(String(250), nullable=True)
